5d9a5d917f32c0886e7d92938aaa20a89630a0f1
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-06-22 20:46:29 +0530

    if something left

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import Navbar from './component/Navbar'
+// import React from 'react'
+// import Navbar from './component/Navbar'
+// import Banner from './component/Banner'
+// import Footer from './component/Footer'
+
+// function App() {
+//   return (
+//     <div >
+
+//       <Navbar/>
+//       <Banner/>
+//       <Footer/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from "react";
+import Navbar from "./component/Navbar";
+import Banner from "./component/Banner";
+import Footer from "./component/Footer";
+import Freebook from "./component/Freebook";
+import Cards from "./component/Card";
 
 function App() {
   return (
-    <div>
-      <Navbar/>
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+      <Banner />
+      <Freebook/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
