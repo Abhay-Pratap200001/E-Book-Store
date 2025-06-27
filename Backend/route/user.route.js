046d7d8e295db7afe596ae0409f835c6570b59f1
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-06-27 18:35:53 +0530

    definig login route

--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -1,7 +1,7 @@
 // Import express to create routes
 import express from "express";
-import { signup } from "../controller/user.controller.js"; // Import the signup controller function
+import { signup, login } from "../controller/user.controller.js"; // Import the signup controller function
 const router = express.Router();  // Create a new router instance
 router.post("/signup", signup);  // Define POST route for user signup
-
+router.post("/login", login);
 export default router;
